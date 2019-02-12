7d4959195be204ee29d48bca00b72ad1966274a2
Will Furnell <mail@willfurnell.com>
2019-02-12 18:51:59 +0000

    apt update too

--- install_deps.sh
+++ install_deps.sh
@@ -2,5 +2,6 @@
 #rm -rf lib/*
 #mkdir -p lib
 #wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
+apt update
 apt install python3-pip -y
 pip3 install --user networkx packaging matplotlib 
