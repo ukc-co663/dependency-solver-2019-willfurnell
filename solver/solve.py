3240aad4204138fb2b490c48ce612a3aaf4f9b0a
Will Furnell <mail@willfurnell.com>
2019-02-14 11:42:03 +0000

    Stop loops, use an actual DB

--- solver/solve.py
+++ solver/solve.py
@@ -1,10 +1,9 @@
 import argparse
 import json
 import networkx as nx
-import sqlite3
 from operator import *
 from packaging import version
-import MySQLdb
+import pymysql.cursors
 
 parser = argparse.ArgumentParser(description='Solve dependencies')
 parser.add_argument('repo', metavar='r', type=str)
@@ -22,10 +21,13 @@ with open(args.initial, 'r') as initial_file:
 with open(args.constraints, 'r') as constraints_file:
     constraints = json.load(constraints_file)
 
-conn = MySQLdb.connect(host="dragon.kent.ac.uk",    # your host, usually localhost
-                     user="wf44",         # your username
-                     passwd="lsar/az",  # your password
-                     db="wf44")        # name of the data base
+# Connect to the database
+conn = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='depsolve',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 #conn = sqlite3.connect(':memory:') # create database in memory
 
@@ -33,8 +35,8 @@ package_db = \
 '''
 CREATE TABLE packages (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(2147483647),
-    version VARCHAR(2147483647),
+    name VARCHAR(255),
+    version VARCHAR(255),
     size INTEGER
 );
 '''
@@ -70,8 +72,16 @@ CREATE TABLE state (
 )
 """
 
-c = conn.cursor()
+unset_for_key_check = "SET foreign_key_checks = 0"
+no_sql_notes = "SET sql_notes = 0"
+set_for_key_check = "SET foreign_key_checks = 1"
+del_pkg = "DROP TABLE IF EXISTS packages, conflicts, depends, state"
 
+c = conn.cursor()
+c.execute(no_sql_notes)
+c.execute(unset_for_key_check)
+c.execute(del_pkg)
+c.execute(set_for_key_check)
 c.execute(package_db)
 c.execute(conflicts_db)
 c.execute(depends_db)
@@ -101,37 +111,37 @@ def parse_constraints(constraints):
         if constraint[0] == "+":
             if "=" in constraint:
                 const = constraint[1:].split("=")
-                c.execute("SELECT id FROM packages WHERE name = ? AND version = ?", [const[0], const[1]])
+                c.execute("SELECT id FROM packages WHERE name = %s AND version = %s", [const[0], const[1]])
                 id = c.fetchone()
-                installs.append(id[0])
+                installs.append(id['id'])
             else:
-                c.execute("SELECT id FROM packages WHERE name = ? ORDER BY version", [constraint[1:]])
+                c.execute("SELECT id FROM packages WHERE name = %s ORDER BY version", [constraint[1:]])
                 id = c.fetchone()
-                installs.append(id[0])
+                installs.append(id['id'])
         else:
             if "=" in constraint:
                 const = constraint[1:].split("=")
-                c.execute("SELECT id FROM packages WHERE name = ? AND version = ?", [const[0], const[1]])
+                c.execute("SELECT id FROM packages WHERE name = %s AND version = %s", [const[0], const[1]])
                 id = c.fetchone()
-                uninstalls.append(id[0])
+                uninstalls.append(id['id'])
             else:
-                c.execute("SELECT id FROM packages WHERE name = ? ORDER BY version", [constraint[1:]])
+                c.execute("SELECT id FROM packages WHERE name = %s ORDER BY version", [constraint[1:]])
                 id = c.fetchone()
-                uninstalls.append(id[0])
+                uninstalls.append(id['id'])
 
     return installs, uninstalls
 
 
 for p in repository:
     # Index repo packages by name and version
-    c.execute("INSERT INTO packages(name, version, size) VALUES (?, ?, ?)", [p['name'], p['version'], p['size']])
+    c.execute("INSERT INTO packages(name, version, size) VALUES (%s, %s, %s)", [p['name'], p['version'], p['size']])
 
 conn.commit()
 
 counter2 = 0
 for p in repository:
-    c.execute("SELECT id FROM packages WHERE name = ? and version = ?", [p['name'], p['version']])
-    id = c.fetchone()[0]
+    c.execute("SELECT id FROM packages WHERE name = %s and version = %s", [p['name'], p['version']])
+    pid = c.fetchone()['id']
     if 'depends' in p.keys():
         for dlist in p['depends']:
             if len(dlist) == 1:
@@ -143,69 +153,73 @@ for p in repository:
                 #print(parse_vstring(dep))
                 package_name, package_version, package_req = parse_vstring(dep)
                 if package_req is not None and package_version is not None:
-                    c.execute("SELECT * FROM packages WHERE name = ?", [package_name])
+                    c.execute("SELECT id, version FROM packages WHERE name = %s", [package_name])
                     packages = c.fetchall()
-                    if packages != []:
-                        packages_rightversion = filter(lambda x: package_req(version.parse(x[2]), version.parse(package_version)), packages)
+                    if len(packages) != 0:
+                        packages_rightversion = filter(lambda x: package_req(version.parse(x['version']), version.parse(package_version)), packages)
                         l = list(packages_rightversion)
                         if len(l) > 0:
-                            depid = sorted(l, key=lambda x: version.parse(x[2]))[0][0]
+                            depid = sorted(l, key=lambda x: version.parse(x['version']))[0]['id']
                             try:
-                                c.execute("INSERT INTO depends(package_id, depend_package_id, must_be_installed) VALUES (?, ?, ?)", [id, depid, must_be_installed])
-                            except sqlite3.IntegrityError:
+                                c.execute("INSERT INTO depends(package_id, depend_package_id, must_be_installed) VALUES (%s, %s, %s)", [pid, depid, must_be_installed])
+                            except pymysql.IntegrityError:
                                 pass
                 else:
-                    c.execute("SELECT * FROM packages WHERE name = ?", [package_name])
+                    c.execute("SELECT id, version FROM packages WHERE name = %s", [package_name])
                     packages = c.fetchall()
-                    if packages != []:
+                    if len(packages) != 0:
                         # We didn't find ANY packages in the repo with this name! That means that we should probably just ignore this dependency is even a thing
-                        depid = sorted(packages, key=lambda x: version.parse(x[2]))[0][0]
+                        depid = sorted(packages, key=lambda x: version.parse(x['version']))[0]['id']
                         try:
-                            c.execute("INSERT INTO depends(package_id, depend_package_id, must_be_installed) VALUES (?, ?, ?)", [id, depid, must_be_installed])
-                        except sqlite3.IntegrityError:
+                            c.execute("INSERT INTO depends(package_id, depend_package_id, must_be_installed) VALUES (%s, %s, %s)", [pid, depid, must_be_installed])
+                        except pymysql.IntegrityError:
                             pass
     conn.commit()
     if 'conflicts' in p.keys():
         for conflict in p['conflicts']:
             package_name, package_version, package_req = parse_vstring(conflict)
             if package_req is not None and package_version is not None:
-                c.execute("SELECT * FROM packages WHERE name = ?", [package_name])
+                c.execute("SELECT id, version FROM packages WHERE name = %s", [package_name])
                 cons = c.fetchall()
                 for con in cons:
-                    if package_req(version.parse(con[2]), version.parse(package_version)):
+                    if package_req(version.parse(con['version']), version.parse(package_version)):
                         try:
-                            c.execute("INSERT INTO conflicts(package_id, conflict_package_id) VALUES (?, ?)", [id, con[0]])
-                        except sqlite3.IntegrityError:
+                            c.execute("INSERT INTO conflicts(package_id, conflict_package_id) VALUES (%s, %s)", [pid, con['id']])
+                        except pymysql.IntegrityError:
                             pass
             else:
-                c.execute("SELECT id FROM packages WHERE name = ?", [package_name])
+                c.execute("SELECT id FROM packages WHERE name = %s", [package_name])
                 cons = c.fetchall()
                 for con in cons:
                     try:
-                        c.execute("INSERT INTO conflicts(package_id, conflict_package_id) VALUES (?, ?)", [id, con[0]])
-                    except sqlite3.IntegrityError:
+                        c.execute("INSERT INTO conflicts(package_id, conflict_package_id) VALUES (%s, %s)", [pid, con['id']])
+                    except pymysql.IntegrityError:
                         pass
     conn.commit()
     counter2 += 1
     print(str(counter2) + " of " + str(len(repository)))
 conn.commit()
 
+
 def add_dep_to_installs(package_id):
-    c.execute("SELECT * FROM depends WHERE package_id = ?", [package_id])
+    c.execute("SELECT depend_package_id FROM depends WHERE package_id = %s", [package_id])
     tmp = c.fetchall() # Only get ID
     dependencies = []
     for d in tmp:
-        G.add_edge(package_id, d[1])
-        dependencies.append(d[1])
+        G.add_edge(package_id, d['depend_package_id'])
+        if d['depend_package_id'] not in installs:
+            dependencies.append(d['depend_package_id'])
     installs.extend(dependencies)
     map(lambda x: add_dep_to_installs(x), dependencies)
 
+
 def add_conflict_to_uninstalls(package_id):
-    c.execute("SELECT * FROM conflicts WHERE package_id = ?", [package_id])
+    c.execute("SELECT conflict_package_id FROM conflicts WHERE package_id = %s", [package_id])
     tmp = c.fetchall()
     conflicts = []
     for con in tmp:
-        conflicts.append(con[1])
+        if con['conflict_package_id'] not in conflicts:
+            conflicts.append(con['conflict_package_id'])
     installs.extend(conflicts)
     map(lambda x: add_dep_to_installs(x), conflicts)
 
@@ -223,9 +237,13 @@ for n in uninstalls:
     print(n)
 
 for n in nx.algorithms.dag.lexicographical_topological_sort(G.reverse()):
-    c.execute("SELECT name, version FROM packages WHERE id = ?", [n])
+    c.execute("SELECT name, version FROM packages WHERE id = %s", [n])
     res = c.fetchone()
-    install_order.append("+" + res[0] + "=" + res[1])
+    install_order.append("+" + res['name'] + "=" + res['version'])
 
 print(json.dumps(install_order))
 
+c.execute(unset_for_key_check)
+c.execute(del_pkg)
+c.execute(set_for_key_check)
+conn.commit()
