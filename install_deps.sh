e9e1fc5d41aa716924f8da5fef20332479c6bc33
Will Furnell <mail@willfurnell.com>
2019-02-16 13:32:05 +0000

    Properly install mariadb

--- install_deps.sh
+++ install_deps.sh
@@ -4,5 +4,5 @@
 #wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
 apt update
 apt install python3-pip -y
-apt install mariadb -y
+apt install mariadb-server mariadb-client -y
 pip3 install --user networkx packaging matplotlib 
